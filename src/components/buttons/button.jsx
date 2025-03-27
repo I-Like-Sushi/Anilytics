cb2020330d08b3c7a977bdf18b896553eddb508d
I-Like-Sushi <deniz.tutak@hotmail.com>
2025-03-27 11:37:52 +0100

    deleted unused button

--- src/components/buttons/button.jsx
+++
@@ -1,9 +0,0 @@
-import "./button.css"
-
-function Button() {
-    return (
-        <button className="random-button" role="button"><span className="text">Random Anime</span></button>
-    )
-}
-
-export default Button;
